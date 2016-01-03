f5464a1d7ce7058956ff8693f0482316d9614ea8
Sven Pfleiderer <sg+github@roothausen.de>
2016-01-03 12:54:35 +0100

    Setup testing tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,32 @@
 const gulp = require('gulp');
 const eslint = require('gulp-eslint');
+const mocha = require('gulp-mocha');
+const istanbul = require('gulp-istanbul');
+
+var istanbulSettings = {
+  dir: 'coverage',
+  reporters: ['html', 'text', 'text-summary'],
+  reportOpts: {
+    html: { dir: 'coverage/html' }
+  }
+};
 
 gulp.task('lint', function () {
   return gulp.src(['**/*.js','!node_modules/**'])
   .pipe(eslint())
   .pipe(eslint.format())
   .pipe(eslint.failAfterError());
+});
+
+gulp.task('pre-test', function () {
+  return gulp.src(['lib/**/*.js'])
+  .pipe(istanbul())
+  .pipe(istanbul.hookRequire());
+});
+
+gulp.task('test', ['pre-test'], function () {
+  return gulp.src('./test/**/*.js')
+  .pipe(mocha())
+  .pipe(istanbul.writeReports(istanbulSettings));
 });
 
