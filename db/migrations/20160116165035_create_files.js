3c5563d010daa36fa2cf713463a8b7875ba4918e
Sven Pfleiderer <sg+github@roothausen.de>
2016-01-16 16:59:53 +0100

    Add migration to create files table

---
+++ db/migrations/20160116165035_create_files.js
@@ -0,0 +1,24 @@
+var table = function (table) {
+  table.uuid('id').primary();
+  table.string('path').unique().notNullable();
+
+  table.integer('size').notNullable();
+  table.string('mime_type').notNullable();
+
+  table.timestamps();
+};
+
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable('files', table)
+  .then(function () {
+    console.log('Files table is created!');
+  });
+};
+
+exports.down = function (knex, Promise) {
+  return knex.schema
+  .dropTable('files', table)
+  .then(function () {
+    console.log('Files table was dropped!');
+  });
+};
