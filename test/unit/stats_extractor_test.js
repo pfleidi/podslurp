48691a780f634a635c878439544aa6187fde2b40
Sven Pfleiderer <sg+github@roothausen.de>
2016-01-31 14:04:31 +0100

    Implement basic user agent parsing

--- test/unit/stats_extractor_test.js
+++ test/unit/stats_extractor_test.js
@@ -10,8 +10,7 @@ describe('statsExtractor', function () {
   const sentBytes = 100;
   const duration = 1337;
 
-  const headers = {};
-  const req = { ip: '127.0.0.1', headers: headers };
+  const req = { ip: '127.0.0.1', headers: {} };
   const res = { statusCode: 200 };
 
   const expectedStatsTemplate = {
@@ -22,16 +21,43 @@ describe('statsExtractor', function () {
     ip_address: req.ip,
     country: '?',
     raw_user_agent: 'unknown',
-    parsed_user_agent: '?',
+    parsed_user_agent: 'Other',
     raw_referrer: 'unknown',
     parsed_referrer: '?'
   };
 
   const exctractStatistics = statsExtractor(req, res);
 
-  it('returns a valid response object', function () {
-    let stats = exctractStatistics('canceled', 1337, 100)
+  describe('without a user agent or a referrer', function () {
+    it('returns a valid response object', function () {
+      let stats = exctractStatistics('canceled', duration, sentBytes)
+      console.dir(req);
+      console.dir(stats);
 
-    assert.deepEqual(stats, expectedStatsTemplate);
+      assert.deepEqual(stats, expectedStatsTemplate);
+    });
   });
+
+  describe('with a known user agent', function () {
+    let userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36';
+
+    before(function () {
+      req.headers['user-agent'] = userAgent;
+    });
+
+    after(function () {
+      req.headers['user-agent'] = null;
+    });
+
+    it('returns a valid response object', function () {
+      let stats = exctractStatistics('canceled', duration, sentBytes)
+      let expectedStats = _.merge(expectedStatsTemplate, {
+        raw_user_agent: userAgent,
+        parsed_user_agent: 'Chrome'
+      });
+
+      assert.deepEqual(stats, expectedStatsTemplate);
+    });
+  });
+
 });
